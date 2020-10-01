16778f899d1be7a12d90197d99a3a9bc7eb6f922
Tomasz Zbrożek <tomasz.zbrozek@o2.pl>
2020-10-01 14:38:06 +0200

    Default controller with Put and Get functions

---
+++ zbrozonoidWebService/Controllers/ValuesController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NLog;
+
+namespace zbrozonoidWebService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public class PadMovement
+        {
+            public int playerId { get; set; }
+            public int movement { get; set; }
+        }
+
+        private static readonly NLog.Logger Logger = LogManager.GetCurrentClassLogger();
+
+        private static readonly IDictionary<int, PadMovement> clientData = new Dictionary<int, PadMovement>();
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<PadMovement> Get(int id)
+        {
+            if (!clientData.Keys.Contains(id))
+            {
+                Logger.Info($"Error - key {id} not found");
+                return new PadMovement();
+            }
+
+            Logger.Info("Get " + id);
+            return clientData[id];
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] PadMovement value)
+        {
+            Logger.Info("Put " + id);
+
+            if (!clientData.Keys.Contains(id))
+            {
+                clientData.Add(id, value);
+            }
+            else
+            {
+                clientData[id] = value;
+            }
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ zbrozonoidWebService/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using NLog;
+
+namespace zbrozonoidWebService
+{
+    public class Program
+    {
+        private static readonly NLog.Logger Logger = LogManager.GetCurrentClassLogger();
+
+        public static void Main(string[] args)
+        {
+            var config = new NLog.Config.LoggingConfiguration();
+            var logfile = new NLog.Targets.FileTarget("logfile") { FileName = "zbrozonoidWebService.log" };
+            config.AddRule(NLog.LogLevel.Info, NLog.LogLevel.Fatal, logfile);
+            config.AddRule(NLog.LogLevel.Debug, NLog.LogLevel.Debug, logfile);
+            LogManager.Configuration = config;
+
+            Logger.Info("start");
+
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
